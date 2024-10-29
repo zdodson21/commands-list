ce8f8f93722cc450d12b807c383e8cca0b60a57c
Zach Dodson <zdodson21@gmail.com>
2024-10-28 20:45:08 -0400

    added support for controls

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+let allowClickToCopy = true;
+let enableAlerts = true;
+
 function copyToClipboard(e) {
-  console.log("Copying code...");
-  const code = e.target.parentElement.querySelector("p");
-  if (code) navigator.clipboard.writeText(code.innerText);
-  console.log(`Copied "${code.innerText}" to clipboard!`);
+  if (allowClickToCopy) {
+    console.log("Copying code...");
+    const code = e.target.parentElement.querySelector("p");
+    if (code) navigator.clipboard.writeText(code.innerText);
+    console.log(`Copied "${code.innerText}" to clipboard!`);
+  
+    if (code.classList.contains("dependency") && enableAlerts) {
+      alert("Please ensure you have the proper install tool before running this command!")
+    }
+  }
 }
