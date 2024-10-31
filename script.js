001590afbf80a67ab65c035f37c403657b86a0f9
Zach Dodson <zdodson21@gmail.com>
2024-10-31 15:14:22 -0400

    added support for copy message notification, documentation

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let allowClickToCopy = true;
 let enableAlerts = true;
 
+/**
+ * Handles key press events
+ */
+function keyPress(e) {
+  if (e.key === "Enter") {
+    copyToClipboard(e);
+  }
+}
+
+/**
+ * Handles copying commands to clipboard
+ */
 function copyToClipboard(e) {
   if (allowClickToCopy) {
     console.log("Copying code...");
@@ -11,10 +23,18 @@ function copyToClipboard(e) {
     if (code.classList.contains("dependency") && enableAlerts) {
       alert("Please ensure you have the proper install tool before running this command!")
     }
+
+    displayCopyNotification();
   }
 }
 
-// Complete function for showing "Copied" notification in corner
-function displayCopyNotification(e) {
-  
+/**
+ * Displays the "Copied" notification for 1 second
+ */
+function displayCopyNotification() {
+  const ALERT = document.querySelector(".copy-message");
+  ALERT.style.display = "inline-block";
+  setTimeout(() => {
+    ALERT.style.display = "none";
+  }, 1000);
 }
